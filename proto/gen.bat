94a74ef32a61eafe228a557d6a4715a6bfdbc0a9
hts0000 <52144113+hts0000@users.noreply.github.com>
2024-07-18 17:41:52 +0800

    update proto file

--- proto/gen.bat
+++ proto/gen.bat
@@ -1,3 +1,5 @@
 protoc --proto_path proto/ --go_out ops-backend/domain/api/gen/v1 --go_opt paths=source_relative --go-grpc_out ops-backend/domain/api/gen/v1 --go-grpc_opt paths=source_relative --proto_path proto/domain domain.proto
 
 protoc --proto_path proto/ --grpc-gateway_out ops-backend/domain/api/gen/v1 --grpc-gateway_opt logtostderr=true --grpc-gateway_opt paths=source_relative --proto_path proto/domain domain.proto
+
+protoc --proto_path proto/ --go_out ops-backend/whereip/api/gen/v1 --go_opt paths=source_relative --go-grpc_out ops-backend/whereip/api/gen/v1 --go-grpc_opt paths=source_relative --proto_path proto/whereip whereip.proto
